75096218d894126c6ee77e6a7172d9cfe240602e
MashiroC <843961670@qq.com>
2019-10-28 14:36:59 +0800

    fix(cmd):remove demo

---
+++ cmd/main.go
@@ -0,0 +1,14 @@
+// Time : 2019/10/28 14:36
+// Author : MashiroC
+
+// cmd
+package main
+
+import begoniarpc "github.com/MashiroC/begonia-rpc"
+
+// main.go something
+
+func main(){
+	center :=begoniarpc.Default()
+	center.Run(":8080")
+}
